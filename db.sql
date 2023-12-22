c4b84b32330e4b21605ef2267eab5c8e9f1cea6c
mohammedfares3 <150060209+mohammedfares3@users.noreply.github.com>
2023-12-22 04:57:00 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,22 @@
+CREATE USER
+IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON array_storage.* TO 'user'@'localhost';
 
+CREATE DATABASE
+IF NOT EXISTS array_storage;
+
+CREATE TABLE
+IF NOT EXISTS array_storage.unsorted_arrays
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    array_data TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE
+IF NOT EXISTS array_storage.sorted_arrays
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    array_data TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
